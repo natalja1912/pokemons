e63df6b8381637e3e77572edbc99f32ef7e092f8
natalja1912@gmail.com <natalja1912@gmail.com>
2021-02-14 17:24:16 +0300

    feat: adaptivity

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,22 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
 import { Link } from "react-router-dom";
 import Wrapper from '../Wrapper/Wrapper';
 import Navigation from '../Navigation/Navigation';
 
 function Header() {
+    const [showMenu, setShowMenu] = useState(false);
+
+    function handleMenuButton() {
+        setShowMenu(prev => !prev);
+    }
+
     return (
         <header className="header">
-            <Wrapper>
+            <Wrapper className="wrapper_no-padding">
                 <div className="header__group">
-                    <div className="header__mobile-group">
-                        <Link to="/" className={`header__logo`}>PokemonsApi</Link>
-                        <button className="header__menu-button header__menu-button_active"></button>
+                    <div className="header__menu">
+                        <Link to="/" className='header__logo'>PokemonsApi</Link>
+                        <button className={`header__menu-button ${!showMenu && `header__menu-button_active`} ${showMenu && `header__menu-button_close`}`} onClick={handleMenuButton}></button>
                     </div>
-                    <Navigation />
+                    <Navigation showMenu={showMenu} />
                 </div>
             </Wrapper>
-        </header>
+        </header >
     );
 }
 
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import './Navigation.css';
 
-function Navigation() {
+function Navigation({ showMenu }) {
+    const location = useLocation();
+
     return (
-        <nav className="nav">
-            <Link to="/" className={`nav__link`}>Главная</Link>
-            <Link to="/my-collection" className={`nav__link`}>Избранное</Link>
+        <nav className={`nav ${showMenu && `nav_active`}`}>
+            <Link to="/" className={`nav__link ${location.pathname === '/' && `nav__link_type_active`}`}>Главная</Link>
+            <Link to="/my-collection" className={`nav__link ${location.pathname === '/my-collection' && `nav__link_type_active`}`}>Избранное</Link>
         </nav>
     );
 }
